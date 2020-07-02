8258817937ed090b105178e130de50372fc835d5
Restyled.io <commits@restyled.io>
2020-07-02 22:33:43 +0000

    Restyled by black

--- 10-Docker-ECS/flask-redis-app/app.py
+++ 10-Docker-ECS/flask-redis-app/app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message=f"Hello! I am a container in {os.environ.get('ENVIRONMENT')}")
+    return jsonify(
+        message=f"Hello! I am a container in {os.environ.get('ENVIRONMENT')}"
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- 10-Docker-ECS/landing-flask/app.py
+++ 10-Docker-ECS/landing-flask/app.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 from flask import Flask, send_from_directory, render_template
 
-app = Flask(__name__, static_url_path='', template_folder='')
+app = Flask(__name__, static_url_path="", template_folder="")
 
 
-@app.route('/js/<path:path>')
+@app.route("/js/<path:path>")
 def send_js(path):
-    return send_from_directory('js', path)
+    return send_from_directory("js", path)
 
 
-@app.route('/img/<path:path>')
+@app.route("/img/<path:path>")
 def send_img(path):
-    return send_from_directory('img', path)
+    return send_from_directory("img", path)
 
 
-@app.route('/css/<path:path>')
+@app.route("/css/<path:path>")
 def send_css(path):
-    return send_from_directory('css', path)
+    return send_from_directory("css", path)
 
 
-@app.route('/fonts/<path:path>')
+@app.route("/fonts/<path:path>")
 def send_fonts(path):
-    return send_from_directory('fonts', path)
+    return send_from_directory("fonts", path)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('/index.html', title='Landing page')
+    return render_template("/index.html", title="Landing page")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
