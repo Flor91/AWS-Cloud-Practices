156a186b9709fe076461d94c7248ebf271cf07f5
Restyled.io <commits@restyled.io>
2020-07-09 23:37:34 +0000

    Restyled by prettier

--- 12-Microservicios/first-lambda.js
+++ 12-Microservicios/first-lambda.js
@@ -1,11 +1,11 @@
-'use strict';
-console.log('Loading hello world function');
+"use strict";
+console.log("Loading hello world function");
 
 exports.handler = async (event) => {
   let name = "you";
-  let city = 'World';
-  let time = 'day';
-  let day = '';
+  let city = "World";
+  let time = "day";
+  let day = "";
   let responseCode = 200;
   console.log("request: " + JSON.stringify(event));
 
@@ -19,22 +19,20 @@ exports.handler = async (event) => {
     city = event.queryStringParameters.city;
   }
 
-  if (event.headers && event.headers['day']) {
+  if (event.headers && event.headers["day"]) {
     console.log("Received day: " + event.headers.day);
     day = event.headers.day;
   }
 
   if (event.body) {
-    let body = JSON.parse(event.body)
-    if (body.time)
-    time = body.time;
+    let body = JSON.parse(event.body);
+    if (body.time) time = body.time;
   }
 
   let greeting = `Good ${time}, ${name} of ${city}.`;
-  if (day)
-    greeting += ` Happy ${day}!`;
+  if (day) greeting += ` Happy ${day}!`;
 
-  let responseBody = {message : greeting, input : event};
+  let responseBody = { message: greeting, input: event };
 
   // The output from a Lambda proxy integration must be
   // in the following JSON object. The 'headers' property
@@ -43,10 +41,10 @@ exports.handler = async (event) => {
   // base64-encoded payload, you must also set the 'isBase64Encoded'
   // property to 'true'.
   let response = {
-    statusCode : responseCode,
-    headers : {"x-custom-header" : "my custom header value"},
-    body : JSON.stringify(responseBody)
+    statusCode: responseCode,
+    headers: { "x-custom-header": "my custom header value" },
+    body: JSON.stringify(responseBody),
   };
-  console.log("response: " + JSON.stringify(response))
+  console.log("response: " + JSON.stringify(response));
   return response;
 };
